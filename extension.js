9f2962c345c94e75a3c3f07615b151559f34e9bb
m^n <2271720+nlpsuge@users.noreply.github.com>
2021-11-13 17:33:53 +0800

    improve title and icon

--- extension.js
+++ extension.js
@@ -12,7 +12,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const WindowPreview = imports.ui.windowPreview;
-const { Clutter, St, Graphene } = imports.gi;
+const { Clutter, St } = imports.gi;
 
 let windowOverlayInjections;
 
@@ -89,141 +89,38 @@ function enable() {
             a.show();
         });
 
-        // this._icon.hide()
-        print('============');
-        print("this._title -> " + this._title);
-        print("this._icon -> " + this._icon);
-        print("title text -> " + this._title.text);
-        const { scaleFactor } = St.ThemeContext.get_for_stage(global.stage);
-        print("scaleFactor -> " + scaleFactor);
-        print("offset -> " + this._title.offset);
-
-        print('this._title.translation_y -> ' + this._title.translation_y);
-        print('this._title.translation_x -> ' + this._title.translation_x);
-        print('this._title.y -> ' + this._title.y);
-        print('this._title.x -> ' + this._title.x);
-        print('this._title.scale_x -> ' + this._title.scale_x);
-        print('this._title.scale_y -> ' + this._title.scale_y);
-
-        const previewScale = this.window_container.scale_x;
-        const [previewWidth, previewHeight, ...others] =
-            this.window_container.allocation.get_size();
-
-        const heightIncrease = Math.floor(previewHeight * (previewScale - 1) / 2);
-        const widthIncrease = Math.floor(previewWidth * (previewScale - 1) / 2);
-
-        print('previewScale -> ' + previewScale);
-
-        print('previewWidth -> ' + previewWidth);
-        print('previewHeight -> ' + previewHeight);
-        print('others -> ' + others);
-
-        const [width, height] =
-            this.window_container.allocation.get_size();
-
-        print('this.window_container.allocation.width -> ' + width);
-        print('this.window_container.allocation.height -> ' + height);
-
-        print('this.windowCenter.x -> ' + this.windowCenter.x);
-        print('this.windowCenter.y -> ' + this.windowCenter.y);  
-
-        const [width1, height1] = this.window_container.get_size();
-        print('this.window_container.get_size.width -> ' + width1);
-        print('this.window_container.get_size.height -> ' + height1);
-
-        print('this.window_container.width -> ' + this.window_container.width);
-        print('this.window_container.height -> ' + this.window_container.height);
-
-        print('this.window_container.x -> ' + this.window_container.x);
-        print('this.window_container.y -> ' + this.window_container.y);
-
-        const [, closeButtonHeight] = this._closeButton.get_preferred_height(-1);
-        print('closeButtonHeight -> ' + closeButtonHeight);
-
-        const [min_width_p, min_height_p, natural_width_p, natural_height_p] 
-            = this.window_container.get_preferred_size();
-        print('min_width_p -> ' + min_width_p);
-        print('min_height_p -> ' + min_height_p);
-        print('natural_width_p -> ' + natural_width_p);
-        print('natural_height_p -> ' + natural_height_p);
-        
-        print('this.metaWindow -> ' + this.metaWindow);
-        print('this.metaWindow.x -> ' + this.metaWindow.x);
-        print('this.metaWindow.y -> ' + this.metaWindow.y);
-        print('this.metaWindow.height -> ' + this.metaWindow.height);
-
-        print('this._closeButton.height -> ' + this._closeButton.height);
-        print('this._title.height -> ' + this._title.height);
-        print('this._icon.height -> ' + this._icon.height);
-        print('this._closeButton.height - this._title.height -> ' + (this._closeButton.height - this._title.height));
-
-        print('this._closeButton.get_preferred_height(-1) -> ' + this._closeButton.get_preferred_height(-1));
-        print('this._title.height -> ' + this._title.height);
-        print('this._icon.get_preferred_height(-1) -> ' + this._icon.get_preferred_height(-1));
-        print('this._closeButton.get_preferred_height(-1) - this._title.height -> ' + (this._closeButton.get_preferred_height(-1) - this._title.height));
-
         const icon_constraints = this._icon.get_constraints();
         for (const constraint of icon_constraints) {
             if (constraint instanceof Clutter.AlignConstraint) {
                 const align_axis = constraint.align_axis;
                 if (align_axis === Clutter.AlignAxis.Y_AXIS) {
                     // TODO config choise 0(top), 0.5(middle), 1(bottom)
+                    // By default, the icon is always on the center
                     constraint.set_factor(0.5);
                 }
             }
         }
-
-        // const title_constraints = this._title.get_constraints();
-        // for (const constraint of title_constraints) {
-        //     if (constraint instanceof Clutter.AlignConstraint) {
-        //         constraint.set_pivot_point(new Graphene.Point({ x: -1, y: -19 }))
-        //     }
-        // }
         
         const title_constraints = this._title.get_constraints();
         for (const constraint of title_constraints) {
             if (constraint instanceof Clutter.BindConstraint) {
                 const coordinate = constraint.coordinate
                 if (coordinate === Clutter.BindCoordinate.Y) {
+                    // By default, the title is always on the bottom, with no offset
                     constraint.set_offset(0)
                 }
             }
         }
-
-        // this._title.set({
-        //     // x: scale,
-        //     y: this._title.x / 0.2,
-        // });
-        
-        // TODO config text entry, default -this._title.height * 2
-        // this._title.set({
-        //     translation_y: -this._title.height * 2
-        // });
-
     });
 
     windowOverlayInjections['_adjustOverlayOffsets'] = injectToFunction(WindowPreview.WindowPreview.prototype, '_adjustOverlayOffsets', function() {
-        const currentScale = this.window_container.scale_x;
-        if (currentScale === 1) {
-            this._title.set({
-                // translation_y: -this._title.height
-            });
-            return;
-        }
-        print('currentScale -> ' + currentScale)
-
-        // TODO config text entry, default -this._title.height * 2
-        // this._title.set({
-        //     translation_y: -this._title.height
-        // });
+        // nothing
     });
 
     // No need to show or hide tittles and close buttons
     windowOverlayInjections['showOverlay'] = overrideFunction(WindowPreview.WindowPreview.prototype, 'showOverlay', function(animate) {
         if (!this._overlayEnabled)
             return;
-
-        print("_overlayShown -> " + this._overlayShown)
 
         if (this._overlayShown)
             return;
@@ -245,9 +142,7 @@ function enable() {
         const origSize = Math.max(width, height);
         const scale = (origSize + activeExtraSize) / origSize;
 
-	    print('scale -> ' + scale)
-
-        // Trigger _adjustOverlayOffsets() via notify::scale-x
+        // Trigger windowPreview#_adjustOverlayOffsets() via notify::scale-x
         this.window_container.ease({
             scale_x: scale,
             scale_y: scale,
@@ -255,9 +150,7 @@ function enable() {
             mode: Clutter.AnimationMode.EASE_OUT_QUAD,
         });
 
-	    print('this._title.translation_y -> ' + this._title.translation_y)
-
-        // this.originalTitleHeight = this._title.height + (-this._title.translation_y)
+        // Trigger the anonymous method of show-chrome in workspace._addWindowClone
         this.emit('show-chrome');
     });
 
