04a50537761b0a74c61baf80aba6c0e6fdbdd1ba
m^n <2271720+nlpsuge@users.noreply.github.com>
2021-11-15 01:09:30 +0800

    1. Convert gtk3 to gtk4 via `gtk4-builder-tool simplify --3to4 --replace prefs.js` 2. Add some working code in prefs.js, now I can see the settings button in the GNOME extension app

--- prefs.js
+++ prefs.js
@@ -1,10 +1,35 @@
+const { Gtk } = imports.gi;
 const Lang = imports.lang;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Convenience = Me.imports.convenience;
 
 const Settings = new Lang.Class({
-    Name: 'AlwaysShowTitlesInOverview.Settings',
+    Name: 'AlwaysShowTitlesInOverviewSettings',
 
-    _init: function () {
+    _init: function() {
+        // Fix: _gtk_style_provider_private_get_settings: assertion 'GTK_IS_STYLE_PROVIDER_PRIVATE (provider)' failed
+        // Gtk.init(null)
 
+        this.load_ui();
     },
 
+    load_ui: function() {
+        log('Loading ui')
+        this._builder = new Gtk.Builder();
+        this._builder.add_from_file(Me.path + '/Settings.ui');
+        this.notebook = this._builder.get_object('settings_notebook');
+
+        log('Loaded ui')
+    }
+    
 });
+
+function buildPrefsWidget() {
+    let settings = new Settings();
+    let notebook = settings.notebook;
+    return notebook;
+}
+
+function init() {
+
+}
