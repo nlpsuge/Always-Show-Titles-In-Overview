234330e664ab9255f2efbfa5f09195a3eabc8d1b
m^n <2271720+nlpsuge@users.noreply.github.com>
2021-11-24 17:23:33 +0800

    try to handle the case of adding more dynamic workspace

--- workspace.js
+++ workspace.js
@@ -11,18 +11,26 @@ let _objectPrototype;
 
 let _settings;
 
+let _nWorkspacesNotifyId;
+
 /**
  * windowPreview._workspace._background is a Widget, so we can just hide it
  *
- * @param   {workspaceView} workspaceView instance of WorkspacesView
+ * @param   {_workspacesViews} _workspacesViews instances of WorkspacesView
  * @returns {void}
  */
-function _show_or_hide_background(workspaceView) {
-    const hide_background = _settings.get_boolean('hide-background');
-    if (hide_background) {
-        workspaceView._background.hide();
-    } else {
-        workspaceView._background.show();
+function _show_or_hide_background(workspacesDisplay) {
+    print('workspacesDisplay: ' + workspacesDisplay);
+    // workspacesDisplay._workspacesViews is undefined
+    // if drag a window to the last workspace to create more workspaces
+    const _workspacesView = workspacesDisplay._workspacesViews[0];
+    for (const workspace of _workspacesView._workspaces) {
+        const hide_background = _settings.get_boolean('hide-background');
+        if (hide_background) {
+            workspace._background.hide();
+        } else {
+            workspace._background.show();
+        }
     }
 }
 
@@ -37,11 +45,21 @@ var ASTIOWorkspace = class {
             'org.gnome.shell.extensions.always-show-titles-in-overview');
         _objectPrototype = new ObjectPrototype.ObjectPrototype()
 
+        WorkspacesView.WorkspacesDisplay.prototype._show_or_hide_background = _show_or_hide_background;
+
         _objectPrototype.injectOrOverrideFunction(WorkspacesView.WorkspacesDisplay.prototype, 'prepareToEnterOverview', true, function(){
-            const _workspacesView = this._workspacesViews[0];
-            for (const workspace of _workspacesView._workspaces) {
-                _show_or_hide_background(workspace);
-            }
+            print("prepareToEnterOverview.... " + this._workspacesViews.length);
+            let workspacesDisplay = this;
+
+            this._show_or_hide_background(workspacesDisplay);
+
+            let workspaceManager = global.workspace_manager;
+
+            _nWorkspacesNotifyId =
+                workspaceManager.connect('notify::n-workspaces',
+                    this._show_or_hide_background.bind(workspacesDisplay));
+
+            print('_nWorkspacesNotifyId -> ' + _nWorkspacesNotifyId);
         });
     }
 
@@ -56,6 +74,11 @@ var ASTIOWorkspace = class {
         if (_objectPrototype) {
             _objectPrototype.removeInjections(WorkspacesView.WorkspacesDisplay.prototype);
             _objectPrototype = null;    
+        }
+
+        if (_nWorkspacesNotifyId) {
+            let workspaceManager = global.workspace_manager;
+            workspaceManager.disconnect(_nWorkspacesNotifyId);
         }
         
     }
