3e9e39ea2cc5b85211a464517e0c806509132ddf
m^n <2271720+nlpsuge@users.noreply.github.com>
2021-11-25 12:48:36 +0800

    hide the workspace background

--- workspace.js
+++ workspace.js
@@ -1,35 +1,22 @@
 
+const { St, GObject } = imports.gi;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const WorkspacesView = imports.ui.workspacesView;
 const Main = imports.ui.main;
 const Overview = imports.ui.overview;
-
-const ObjectPrototype = Me.imports.utils.objectPrototype
-
-let _objectPrototype;
+const Workspace = imports.ui.workspace;
 
 let _settings;
 
-let _nWorkspacesNotifyId;
-
-/**
- * windowPreview._workspace._background is a Widget, so we can just hide it
- *
- * @param   {_workspacesViews} _workspacesViews instances of WorkspacesView
- * @returns {void}
- */
-function _show_or_hide_background(workspacesDisplay) {
-    print('workspacesDisplay: ' + workspacesDisplay);
-    // workspacesDisplay._workspacesViews is undefined
-    // if drag a window to the last workspace to create more workspaces
-    const _workspacesView = workspacesDisplay._workspacesViews[0];
-    for (const workspace of _workspacesView._workspaces) {
-        const hide_background = _settings.get_boolean('hide-background');
-        if (hide_background) {
-            workspace._background.hide();
-        } else {
-            workspace._background.show();
+function _show_or_hide_workspace_background() {
+    const hide_background = _settings.get_boolean('hide-background');
+    if (hide_background) {
+        Workspace.OriginalWorkspaceBackground = Workspace.WorkspaceBackground;
+        Workspace.WorkspaceBackground = EmptyWorkspaceBackground;
+    } else {
+        if (Workspace.OriginalWorkspaceBackground) {
+            Workspace.WorkspaceBackground = Workspace.OriginalWorkspaceBackground;
         }
     }
 }
@@ -43,24 +30,11 @@ var ASTIOWorkspace = class {
     enable() {
         _settings = ExtensionUtils.getSettings(
             'org.gnome.shell.extensions.always-show-titles-in-overview');
-        _objectPrototype = new ObjectPrototype.ObjectPrototype()
 
-        WorkspacesView.WorkspacesDisplay.prototype._show_or_hide_background = _show_or_hide_background;
-
-        _objectPrototype.injectOrOverrideFunction(WorkspacesView.WorkspacesDisplay.prototype, 'prepareToEnterOverview', true, function(){
-            print("prepareToEnterOverview.... " + this._workspacesViews.length);
-            let workspacesDisplay = this;
-
-            this._show_or_hide_background(workspacesDisplay);
-
-            let workspaceManager = global.workspace_manager;
-
-            _nWorkspacesNotifyId =
-                workspaceManager.connect('notify::n-workspaces',
-                    this._show_or_hide_background.bind(workspacesDisplay));
-
-            print('_nWorkspacesNotifyId -> ' + _nWorkspacesNotifyId);
+        _settings.connect('changed::hide-background', (settings) => {
+            _show_or_hide_workspace_background();
         });
+        _show_or_hide_workspace_background();
     }
 
     // Destroy the created object
@@ -71,16 +45,18 @@ var ASTIOWorkspace = class {
             _settings = null;
         }
 
-        if (_objectPrototype) {
-            _objectPrototype.removeInjections(WorkspacesView.WorkspacesDisplay.prototype);
-            _objectPrototype = null;    
+        if (Workspace.OriginalWorkspaceBackground) {
+            Workspace.WorkspaceBackground = Workspace.OriginalWorkspaceBackground;
+            Workspace.OriginalWorkspaceBackground = null;
         }
-
-        if (_nWorkspacesNotifyId) {
-            let workspaceManager = global.workspace_manager;
-            workspaceManager.disconnect(_nWorkspacesNotifyId);
-        }
-        
     }
 
-} 
+}
+
+var EmptyWorkspaceBackground = GObject.registerClass(
+class WorkspaceBackground extends St.Widget {
+    _init(metaWorkspace, monitorIndex, overviewAdjustment)  {
+        super._init();
+    }
+});
+
