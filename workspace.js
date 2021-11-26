7b9477aaca654358482310c0ebeab26fa6ac3de8
m^n <2271720+nlpsuge@users.noreply.github.com>
2021-11-26 18:53:05 +0800

    Titles and closeButtonsanimate from Overview

--- workspace.js
+++ workspace.js
@@ -1,5 +1,5 @@
 
-const { St, GObject } = imports.gi;
+const { Clutter, St, GObject } = imports.gi;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const WorkspacesView = imports.ui.workspacesView;
@@ -7,7 +7,11 @@ const Main = imports.ui.main;
 const Overview = imports.ui.overview;
 const Workspace = imports.ui.workspace;
 
+const ObjectPrototype = Me.imports.utils.objectPrototype;
+
+const WINDOW_OVERLAY_FADE_TIME = 200;
 let _settings;
+let _objectPrototype;
 
 function _show_or_hide_workspace_background() {
     const hide_background = _settings.get_boolean('hide-background');
@@ -21,6 +25,21 @@ function _show_or_hide_workspace_background() {
     }
 }
 
+function _animateFromOverview(windowPreview, animate) {
+    if (!windowPreview._workspace.metaWorkspace.active) {
+        return;
+    }
+    const toHide = [windowPreview._title, windowPreview._closeButton];
+    toHide.forEach(a => {
+        a.opacity = 255;
+        a.ease({
+            opacity: 0,
+            duration: animate ? WINDOW_OVERLAY_FADE_TIME : 0,
+            mode: Clutter.AnimationMode.EASE_OUT_EXPO
+        });
+    });
+}
+
 var CustomWorkspace = class {
 
     constructor() {
@@ -30,11 +49,19 @@ var CustomWorkspace = class {
     enable() {
         _settings = ExtensionUtils.getSettings(
             'org.gnome.shell.extensions.always-show-titles-in-overview');
+        _objectPrototype = new ObjectPrototype.ObjectPrototype();
 
         _settings.connect('changed::hide-background', (settings) => {
             _show_or_hide_workspace_background();
         });
         _show_or_hide_workspace_background();
+
+        _objectPrototype.injectOrOverrideFunction(Workspace.Workspace.prototype, 'prepareToLeaveOverview', true, function() {
+            for (let i = 0; i < this._windows.length; i++) {
+                const windowPreview = this._windows[i];
+                _animateFromOverview(windowPreview, true);
+            }
+        });
     }
 
     // Destroy the created object
@@ -48,6 +75,11 @@ var CustomWorkspace = class {
         if (Workspace.OriginalWorkspaceBackground) {
             Workspace.WorkspaceBackground = Workspace.OriginalWorkspaceBackground;
             Workspace.OriginalWorkspaceBackground = null;
+        }
+
+        if (_objectPrototype) {
+            _objectPrototype.removeInjections(Workspace.Workspace.prototype);
+            _objectPrototype = null;
         }
     }
 
